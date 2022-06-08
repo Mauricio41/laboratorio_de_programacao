658ba00f4ba29cdfa08c8b00db62f8f4ec1ffe91
Emanuel Avila <76269418+meiazero@users.noreply.github.com>
2022-06-08 14:35:36 -0300

    base code.

--- arrays.c
+++ arrays.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+/* The questions must be asked from the first to the last, in ascending order, from bottom to top. */
+int questoes(){
+    int quest, key;
 
+    printf("Escolha uma questão:\n");
+    scanf("%d", &quest);
+    do{
+      switch (quest){
+    case 0:
+        printf("Saindo...\n");
+        key = 1;
+        break;
+    case 1:
+        questao1();
+        key = 0;
+        break;
+    /* add news questions to the bottom.
+    Structure:
+    case [number]:
+        [question_number]();
+        key = 0;
+        break; */
+    default:
+        printf("Questao invalida!");
+        break;
+    }  
+    } while (key);
+    
+
+    return 0;
+}
 int main(){
-    int a = 10;
-    printf("%d\n", a);
+
+    questoes();
+
+    return 0;
 }
